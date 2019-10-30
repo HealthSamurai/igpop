b98a23a130c5fbbde5e25b67d65c7ebe78fd8b65
VictorGus <retaow@gmail.com>
2019-10-30 15:37:55 +0300

    valueset menu

--- src/src/igpop/site/valuesets.clj
+++ src/src/igpop/site/valuesets.clj
@@ -21,3 +21,33 @@
                                       display]
                                      [:div.column
                                       "???"]]) concepts)))
+
+(defn menu-vs [ctx {uri :uri}]
+  (letfn [(current-page [uri res-url] (= uri res-url))]
+    [:div#main-menu
+     (for [vs (keys (:valuesets ctx))] 
+       [:div
+        (let [res-url (str "/valuesets/" (name vs))]
+          [:a {:href res-url :class (when (current-page uri res-url) "active")} (name vs)])])]))
+
+(defn valuesets-dashboard [ctx req]
+  {:status 200
+   :body (views/layout
+          [:div#main-menu
+           [:a {:href "/valuesets/patient-identity"} "patient-identity"]]
+          [:div#content
+           [:h1 "Valuesets"]])})
+
+(defn valueset [ctx {{vid :valuset-id} :route-params :as req}]
+  (let [vs (get-in ctx [:valuesets (keyword vid)])
+        description (get vs :description)]
+    {:status 200
+     :body (views/layout
+            (menu-vs ctx req)
+            [:div#content [:h1 "Valueset " vid]
+             [:div.summary description]
+             [:hr]
+             [:br]
+             (render-tb-vs vs)])}))
+
+
