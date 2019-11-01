07d06d8dd1f043754b09aa9e0d403aae42785a38
Ivan Mikhailov <ivnmikhailov@gmail.com>
2019-11-01 13:23:47 +0300

    menu view script

--- src/resources/public/collapse.js
+++ src/resources/public/collapse.js
@@ -2,10 +2,5 @@ $(".el-header").click(function () {
 
     $header = $(this);
     $content = $header.next();
-    $content.slideToggle(500, function () {
-        //$header.text(function () {
-            //return $content.is(":visible") ? "Collapse" : "Expand";
-        //});
-    });
-
+    $content.slideToggle(500, function () {});
 });
