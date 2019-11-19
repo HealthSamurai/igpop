558d511bd0ec252fb871b83af4aff275a5e1a001
Ivan Mikhailov <ivnmikhailov@gmail.com>
2019-11-19 19:48:15 +0300

    favicon update

--- src/igpop/site/views.clj
+++ src/igpop/site/views.clj
@@ -183,7 +183,7 @@
                      :integrity "sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
                      :crossorigin "anonymous"}]
 
-             [:link {:rel "icon" :href (u/href ctx "static" "icon.png")}] 
+             [:link {:rel "icon" :href (u/href ctx "static" "fire-solid.png")}] 
              [:link {:href "//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" :rel "stylesheet"}]
              [:link {:href "//fonts.googleapis.com/css?family=Montserrat|Roboto&display=swap" :rel "stylesheet"}]
              [:style style]
