5a1a06cea7e026c2808890528f69ed44dc50b3a1
Ivan Mikhailov <ivnmikhailov@gmail.com>
2019-11-19 15:54:21 +0300

    profile animation time fixed

--- src/igpop/site/profiles.clj
+++ src/igpop/site/profiles.clj
@@ -64,10 +64,10 @@
        :color "rgb(33,37,41)"
        :font-weight "400"
        :max-height "1400px"
-       :-webkit-transition "max-height 0.5s ease-in-out"
-       :-moz-transition "max-height 0.5s ease-in-out"
-       :-o-transition "max-height 0.5s ease-in-out"
-       :transition "max-height 0.5s ease-in-out"}
+       :-webkit-transition "max-height 0.3s ease-in-out"
+       :-moz-transition "max-height 0.3s ease-in-out"
+       :-o-transition "max-height 0.3s ease-in-out"
+       :transition "max-height 0.3s ease-in-out"}
 
       [:.required {:color "red" :opacity 0.7 :margin "0 0.2em"}]
       [:.coll {:color "#888"}]
