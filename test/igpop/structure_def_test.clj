0058f897bafa723af5b9d4831dd52eeef6e75861
Ivan Mikhailov <ivnmikhailov@gmail.com>
2019-12-18 16:56:39 +0300

    test(structure-def): add agenda and cardinality test

--- test/igpop/structure_def_test.clj
+++ test/igpop/structure_def_test.clj
@@ -21,6 +21,38 @@
               (sdef/to-sd profile)
               {:snapshot [{:path "Patient.name" :min 1}]})))
 
+  (def plain-elements-in
+    {:CarePlan.subject {}
+     :CarePlan.text {}})
+
+  (testing "elements-to-sd func"
+    (matcho/match
+     (sdef/elements-to-sd plain-elements-in)
+                          [{:id "CarePlan.subject",
+                            :path "CarePlan.subject"},
+                           {:id "CarePlan.text",
+                            :path "CarePlan.text"}]))
+
+  (testing "cardinality required"
+    (matcho/match
+     (sdef/cardinality :required true)
+     {:min 1}))
+
+  (testing "cardinality disabled"
+    (matcho/match
+     (sdef/cardinality :disabled true)
+     {:max 0}))
+
+  (testing "cardinality minItems"
+    (matcho/match
+     (sdef/cardinality :minItems 5)
+     {:min 5}))
+
+  (testing "cardinality maxItems"
+    (matcho/match
+     (sdef/cardinality :maxItems 14)
+     {:max 14}))
+
 
 
   (def project-path (.getPath (io/resource "test-project")))
