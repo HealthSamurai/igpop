c1c72ff2b0ab816f33a40f851f2c26f10dbaf8bd
qdzo <banch.idea19@gmail.com>
2019-12-21 14:51:59 +0300

    Add transforms for: constraints,constant,cardinality,polymorphic
    Add tests.
    
    NOTE: This is draft only. Need to determine better dispatch logic

--- src/igpop/structure_def.clj
+++ src/igpop/structure_def.clj
@@ -46,5 +46,223 @@
                                                                (assoc :snapshot (generate-snapshot))
                                                                (assoc :differential (generate-differential rt prn props))))))))))))
 
-(defn to-sd [igpop]
-  {:snapshot {:element [{:path "Patient.name"}]}})
+
+;; ----------------------------------------------------------------------------------------
+
+(require '[clojure.string :as s])
+
+(set! *warn-on-reflection* true)
+
+;; ---------------------------------------------------------------------
+
+(defn capitalize
+  "Uppercase first character of the string.
+  Unlike `clojure.string/capitalize` - we don't lowercase rest of the string"
+  [^String s]
+  (str (.toUpperCase (subs s 0 1)) (subs s 1)))
+
+(defn to-sd-path [parts]
+  (s/join "." (map name parts)))
+
+(defn poly-name-to-path-x
+  "Convert path `parts` to path-x of polymorphic value
+  Example: `[:A,:B,:C] -> A.B.C[x]`"
+  [path] (str (to-sd-path path) "[x]"))
+
+(defn poly-name-to-path
+  "Convert path `parts` and `poly-value` to path of polymorphic value
+  Example: `[:A,:B,:C] and value -> A.B.CValue`"
+  [path poly-value]
+  (str (to-sd-path path) (capitalize (name poly-value))))
+
+;; ---------------------------------------------------------------------
+
+(defn process-description [item]
+  "Rename :description key to :human"
+  (-> item
+      (assoc :human (:description item))
+      (dissoc :description)))
+
+;; (process-description {:description "Hello"}) 
+
+
+;; convert igpop constraints
+;; ```
+;; elements:
+;;   name:
+;;     constaints:
+;;       us-core-8:
+;;         expression: "family.exists() or given.exists()"
+;;         description: "Patient.name.given or Patient.name.family or both SHALL be present"
+;;         # severity: error (default)
+;; ```
+;;
+;; into structure-definition constraints
+;;
+;; ```
+;; constraints:
+;; - severity: error
+;;   key: us-core-8
+;;   expression: "family.exists() or given.exists()"
+;;   human: "Patient.name.given or Patient.name.family or both SHALL be present"
+;; ```
+(defn ig-constraint->sd-constraint
+  [[item-name item]]
+  (cond-> item
+    :default                (assoc :key (name item-name))
+    (:description item)     (process-description)
+    (nil? (:severity item)) (assoc :severity "error")
+    ;; TODO: which atrributes we need to process here?
+    ))
+
+(defn process-constraints [item]
+  "Process 'constraint' igpop logic on item"
+  (-> item
+      (assoc :constraint (mapv ig-constraint->sd-constraint (:constraints item)))
+      (dissoc :constraints)))
+
+(defn cardinality-given?
+  "Check are cardinality keys given"
+  [item] (or (contains? item :disabled)
+             (contains? item :required)
+             (contains? item :minItem)
+             (contains? item :maxItem)))
+
+;; Cardinality-patterns should be checked earlier
+;; we don't check XOR (exactly one) and try to find any of them: (and override values)
+;;   - disabled true
+;;   - required true
+;;   - min and max
+(defn process-cardinality [item]
+  "Process 'cardinality' igpop logic on item"
+  (cond-> item
+    (:disabled item) (assoc :max 0)
+    (:required item) (assoc :min 1)
+    (:minItem  item) (assoc :min (:minItem item))
+    (:maxItem  item) (assoc :max (:maxItem item))
+    :default         (dissoc :disabled :required :minItem
+                             :maxItem :collection)))
+
+(defn process-constant
+  "Process 'constant' igpop logic on item"
+  [item]
+  (let [key (keyword (str "fixed" (:type item)))]
+    (-> item
+        (assoc key (:constant item))
+        (dissoc :constant))))
+
+;; Convert igpop Polymorphic types
+;; ```
+;; elements:
+;;   value:
+;;     required: true
+;;     union: [string CodeableConcept Quantity]
+;;     string: {required: true}
+;;     CodeableConcept:
+;;       required: true
+;;       valueset {id: 'vs'}
+;; ```
+;;
+;; into several structure-definition polymorphic entries
+;;
+;; ```
+;; - path: Observation.value[x]
+;;   slicing:
+;;     discriminator:
+;;     - type: type
+;;     path: "$this"
+;;     ordered: false
+;;     rules: closed
+;;   type:
+;;   - code: Quantity
+;;   - code: string
+;; - path: Observation.valueCodeableConcept
+;;   binding: {valuset: 'http://....'}
+;; ...
+;; - path: Observation.valueString
+;; ```
+
+(defn process-valueset [item]
+  "Move valueset key-val to `:binding` attribute"
+  (-> item
+      ;; what keys also need to put into :binding?
+      (update :binding merge (select-keys item [:valueset]))
+      (dissoc :valueset)))
+
+;; (process-valueset {:valueset {:id "none"}})
+
+;; can't find good name for this fn.
+(defn poly-value-item [path [poly-name item]]
+  (cond-> item
+    :default         (assoc :path (poly-name-to-path path poly-name))
+    (:valueset item) (process-valueset)))
+
+(defn igpop-polymorphic->sd-polymorphic
+  [{:keys [path]} item-name item]
+  (let [union-types (:union item)
+        union-defined (select-keys item (mapv keyword union-types))
+        new-path (conj path item-name)]
+    (vec (cons {:type (mapv (fn [t] {:code t}) union-types)
+                :path (poly-name-to-path-x new-path)
+                :slicing {:discriminator {:type "type"}
+                          :path "$this"
+                          :ordered false
+                          :rules "closed"}}
+               (mapv (partial poly-value-item new-path) union-defined)))))
+
+;; ;; (assert (=
+         ;; (igpop-polymorphic->sd-polymorphic
+         ;;  {:path [:Observation]}
+         ;;  (key (first igpop-polymorphic-type))
+         ;;  (val (first igpop-polymorphic-type)))
+;; ;;          sd-polymorphic-types))
+
+(defn ig-item->sd-item [ctx item-name item]
+  "Convert `ig-pop-item` to `structure-definition-item`"
+  (cond-> item
+    :default                   (assoc :path (to-sd-path (conj (:path ctx) item-name)))
+    (nil? (:mustSupport item)) (assoc :mustSupport true)
+    (cardinality-given? item)  (process-cardinality)
+    (:constant item)           (process-constant)
+    (:constraints item)        (process-constraints)))
+
+;; ---------------------------------------------------------------------
+
+(defn to-sd-elements
+  "Walk on elements converts items to structure definitions
+   if there are some nested elements, go deeper (recursive function)
+
+  returns context:
+   {:path [path of current position]
+    :result [converted elements]} "
+  [ctx elements]
+  (reduce (fn [ctx [item-name item]]
+            (cond-> ctx
+
+              (:union item)
+              (update :result into (igpop-polymorphic->sd-polymorphic ctx item-name item))
+
+              ;; not a union and not top-level item
+              (and (not (:union item)) (not= (:path ctx) []))
+              (update :result conj (ig-item->sd-item ctx item-name (dissoc item :elements)))
+
+              ;; nested elements
+              (:elements item)
+              (-> (update :path conj item-name)
+                  (to-sd-elements (:elements item))
+                  (assoc :path (:path ctx)))))  ;; reset path before next iteration
+          ctx elements))
+
+;; reset path to original version (:elements add parts to path)
+;; ---------------------------------------------------------------------
+
+(defn to-sd
+  "Transform igpop nested structure to flat vector of
+  structure-definitions and wraps them in `{:snapshot [definitions]}`"
+  [igpop]
+  {:snapshot (-> {:path [] :result []}
+                 (to-sd-elements igpop)
+                 :result)})
+
+;; (to-sd-path [:Patient :name])
+
--- test/igpop/structure_def_test.clj
+++ test/igpop/structure_def_test.clj
@@ -12,15 +12,109 @@
     {:Patient
      {:elements
       {:name {:type "HumanName"
-              :minItem 1
+              :required true
               :elements
               {:family {:type "string" :isCollection true :minItem 1 :maxItem 10}}}}}})
 
-  (comment (testing "collection cardinality"
-             (matcho/match
-              (sdef/to-sd profile)
-              {:snapshot [{:path "Patient.name" :min 1}]})))
+  (testing "cardinality: entity `required`"
+    (matcho/match
+     (sdef/to-sd profile)
+     {:snapshot [{:path "Patient.name" :min 1}]}))
 
+  (testing "cardinality: entity `disabled`"
+    (matcho/match
+     (sdef/to-sd {:Patient {:elements {:name {:disabled true :type "HumanName"}}}})
+     {:snapshot [{:path "Patient.name" :max 0}]}))
+
+  (testing "cardinality: collection `min` and `max`"
+    (matcho/match
+     (sdef/to-sd profile)
+     {:snapshot [{} {:path "Patient.name.family" :min 1 :max 10}]}))
+
+  (testing "`mustSupport`: default  value"
+    (matcho/match
+     (sdef/to-sd profile)
+     {:snapshot [{:mustSupport true}]}))
+
+  (testing "`mustSupport`: override value"
+    (matcho/match
+     (sdef/to-sd {:Patient {:elements {:name {:type "HumanName" :mustSupport false}}}})
+     {:snapshot [{:mustSupport false}]}))
+
+  (testing "path: Correct `path`(nesting) construction"
+    (matcho/match
+     (sdef/to-sd profile)
+     {:snapshot [{:path "Patient.name"} {:path "Patient.name.family"}]}))
+
+  (testing "constant: correct key definition"
+    (matcho/match
+     (sdef/to-sd {:Patient {:elements {:name {:type "HumanName" :constant "Rich"}}}})
+     {:snapshot [{:fixedHumanName "Rich"}]}))
+
+  (def observation-profile
+    {:Observation
+     {:elements
+      {:name
+       {:constraints
+        {:us-core-8 {:expression "family.exists() or given.exists()"
+                     :description "Patient.name.given or Patient.name.family  or both SHALL be present"}}}}}})
+
+  (testing "constraint: default value for `severity`"
+    (matcho/match
+     (sdef/to-sd observation-profile)
+     {:snapshot [{:constraint [{:severity "error"}]}]}))
+
+  (testing "constraint: correct key value"
+    (matcho/match
+     (sdef/to-sd observation-profile)
+     {:snapshot [{:constraint [{:key "us-core-8"}]}]}))
+
+  (testing "constraint: description -> human field"
+    (matcho/match
+     (sdef/to-sd observation-profile)
+     {:snapshot [{:constraint
+                  [{:human "Patient.name.given or Patient.name.family  or both SHALL be present"}]}]}))
+
+  (testing "constraint: expression -> human field"
+    (matcho/match
+     (sdef/to-sd observation-profile)
+     {:snapshot [{:constraint [{:expression  "family.exists() or given.exists()"
+                                :human  "Patient.name.given or Patient.name.family  or both SHALL be present"}]}]}))
+
+  (def polymorphic-type
+    {:Observation
+     {:elements
+      {:value {:required true
+               :union ["string" "CodeableConcept" "Quantity"]
+               :string {:required true}
+               :CodeableConcept {:required true
+                                 :valueset {:id "vs"}}}}}})
+
+  (testing "polymorphic: created item with `[x]` path and discriminator"
+    (matcho/match
+     (sdef/to-sd polymorphic-type)
+     {:snapshot
+      [{:path "Observation.value[x]"
+        :slicing {:discriminator {:type "type"}
+                  :path "$this"
+                  :ordered false
+                  :rules "closed"} }]}))
+
+  (testing "polymorphic: created item `[x]` with correct type"
+    (matcho/match
+     (sdef/to-sd polymorphic-type)
+     {:snapshot [{:type [{:code "string"},{:code "CodeableConcept"},{:code "Quantity"}]}]}))
+
+  (testing "polymorphic: create items for union-types"
+    (matcho/match
+     (sdef/to-sd polymorphic-type)
+     {:snapshot [{} {:path "Observation.valueString"}
+                    {:path "Observation.valueCodeableConcept"}]}))
+
+  (testing "polymorphic: create items with valueset placed in binding"
+    (matcho/match
+     (sdef/to-sd polymorphic-type)
+     {:snapshot [{} {} {:binding {:valueset {:id "vs"}}}]}))
 
 
   (def project-path (.getPath (io/resource "test-project")))
@@ -39,5 +133,6 @@
 
     (spit (io/file (str (System/getProperty "user.dir") "/test-structure-def.json")) (generate-string (sdef/generate-structure project) {:pretty true}))
 
-    ))
+    )
+  )
 
