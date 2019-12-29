5d8f57729ae136fc5c4877903e3faabe071cb32c
Ivan Mikhailov <ivnmikhailov@gmail.com>
2019-12-29 15:23:59 +0300

    feat(monaco-UI): progress

--- resources/public/editor/scripts.js
+++ resources/public/editor/scripts.js
@@ -23,3 +23,21 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}    
+
+docReady(function() {
+    let url = window.location.href;
+    let temp = url.split("/").pop();
+    let resource = temp.split("-").reverse().pop();
+    let profile = temp.split("-").pop();
+    whereami.innerText = resource + ": " + profile;
+});
