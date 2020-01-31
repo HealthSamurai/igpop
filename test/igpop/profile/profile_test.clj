feb0e2aff0899b3dd5cf9cbb3775015b031ce9cd
ildar.fatykhov <ildar.fatykhov@onefactor.com>
2020-01-31 16:59:49 +0300

    refactoring

--- test/igpop/profile/profile_test.clj
+++ test/igpop/profile/profile_test.clj
@@ -1,5 +1,7 @@
 (ns igpop.profile.profile-test
-  (:require [igpop.profile.profile :as prof]
+  (:require [igpop.profile.translate-profile :as prof]
+            [igpop.profile.parse :as parse]
+            [igpop.profile.to-json-rule :as json-rule]
             [clojure.pprint :as p]
             [clojure.test :refer :all]
             [matcho.core :as matcho]
@@ -11,26 +13,31 @@
 (deftest test-profiles
 
   (def profiles {:Patient {:profile-id {:elements    {:identifier {:a 1 :b 2}
-                                                      :extension  {:ex-1 1
-                                                                   :ex-2 2}}
+                                                      :extension  {:ex-1 {:elements {:e11 {:type "some"}}}}}
                                         :description "some description"}}})
+
+  (testing "test extractet 3 resources to parse"
+    (matcho/match
+      (count (parse/denormalize-document :Patient :profile-id profiles))
+      2))
 
   (testing "test extract resources to parse"
     (matcho/match
-      (prof/extract-resources-to-parse :Patient :profile-id profiles)
-      [{:elements     {:identifier {:a 1 :b 2}
-                       :extension  {:ex-1 1 :ex-2 2}}
-        :profile-type :Patient
-        :type         :DomainResource}
-       {:elements     {:ex-1 1}
-        :profile-type :Extension
-        :type         :Extension}
-       {:elements     {:ex-2 2}
-        :profile-type :Extension
-        :type         :Extension}]))
+      (parse/denormalize-document :Patient :profile-id profiles)
+      [{:elements     {:identifier     {:a 1, :b 2},
+                       :extension:ex-1 {:sliceName "ex-1", :extension-reference "ex-1"}},
+        :profile-type :Patient,
+        :type         :DomainResource,
+        :id           "profile-id"}
+       {:elements     {:e11           {:sliceName :e11, :definition nil, :type "Extension"}
+                       :e11.url       {:minItems 1, :maxItems "1", :type "uri", :fixedUri :e11}
+                       :e11.valueSome {:minItems 1, :maxItems "1", :type "some"}
+                       nil            {:definition nil}
+                       :url           {:minItems 1, :maxItems "1", :type "uri", :fixedUri :e11}
+                       "value[x]"     {:minItems 0, :maxItems "0"}},
+        :profile-type :Extension, :type :Extension, :id "ex-1"}]))
 
-  (def elements {
-                 :extension  {:extension1 {:description "Some extension 1"
+  (def elements {:extension  {:extension1 {:description "Some extension 1"
                                            :elements    {:e1 {:required    true
                                                               :description "Some extension 1 text"
                                                               :type        "string"}
@@ -51,182 +58,109 @@
 
   (testing "test translating domain resource to structure definition"
     (matcho/match
-      (prof/flatten-profile elements [:Patient])
-      (ordered-map {[:Patient :extension :extension1] {:extension {:name  "extension1"
-                                                                   :id    [:Patient :extension :extension1]
-                                                                   :entry {:description "Some extension 1"
-                                                                           :elements    {:e1 {:required    true
-                                                                                              :description "Some extension 1 text"
-                                                                                              :type        "string"}
-                                                                                         :e2 {:collection true
-                                                                                              :minItems   1
-                                                                                              :type       "Type"
-                                                                                              :valueset   {:id "some-ext"}}}}}}
-                    [:Patient :identifier]            {:minItems 1}
-                    [:Patient :identifier :system]    {:required true}
-                    [:Patient :identifier :value]     {:required    true
-                                                       :description "The value that is unique within the system"}
-                    [:Patient :name]                  {:minItems    1
-                                                       :description "A name associated with the patient"
-                                                       :rule        {:r1 "family.count() + given.count() >= 1"}}
-                    [:Patient :name :family]          {} [:Patient :name :given] {}})))
-
-
+      (parse/->intermediate-representation elements [:Patient])
+      {[:Patient :extension]          {:extension1 {:description "Some extension 1",
+                                                    :elements    {:e1 {:required    true,
+                                                                       :description "Some extension 1 text",
+                                                                       :type        "string"},
+                                                                  :e2 {:collection true,
+                                                                       :minItems   1,
+                                                                       :type       "Type",
+                                                                       :valueset   {:id "some-ext"}}}}},
+       [:Patient :identifier]         {:minItems 1},
+       [:Patient :identifier :system] {:required true},
+       [:Patient :identifier :value]  {:required    true,
+                                       :description "The value that is unique within the system"},
+       [:Patient :name]               {:minItems 1, :description "A name associated with the patient",
+                                       :rule     {:r1 "family.count() + given.count() >= 1"}},
+       [:Patient :name :family]       {},
+       [:Patient :name :given]        {}}))
+  ;
   (def domain-resource
-    (ordered-map {
-                  [:Patient :extension :extension1] {:extension {:name  "extension1"
-                                                                 :id    [:Patient :extension :extension1]
-                                                                 :entry {:description "Some extension 1"
-                                                                         :elements    {:e1 {:required    true
-                                                                                            :description "Some extension 1 text"
-                                                                                            :type        "string"}
-                                                                                       :e2 {:collection true
-                                                                                            :minItems   1
-                                                                                            :type       "Type"
-                                                                                            :valueset   {:id "some-ext"}}}}}}
-                  [:Patient :identifier]            {:minItems 1}
-                  [:Patient :identifier :system]    {:required true}
-                  [:Patient :identifier :value]     {:required    true
-                                                     :description "The value that is unique within the system"}
-                  [:Patient :name]                  {:minItems    1
-                                                     :description "A name associated with the patient"
-                                                     :rule        {:r1 "family.count() + given.count() >= 1"}}
-                  [:Patient :name :family]          {} [:Patient :name :given] {}}))
-
+    (ordered-map {[:Patient :address]                 {},
+                  [:Patient :address :line]           {},
+                  [:Patient :address :city]           {},
+                  [:Patient :address :postalCode]     {:description "US Zip Codes"},
+                  [:Patient :address :state]          {:valueset {:id "us-core-usps-state"}},
+                  [:Patient :extension:race]          {:sliceName           "race",
+                                                       :extension-reference "race"},
+                  [:Patient :name]                    {:minItems    1,
+                                                       :description "A name associated with the patient",
+                                                       :rule        {:r1 "family.count () + given.count () >= 1"}},
+                  [:Patient :name :family]            {},
+                  [:Patient :name :given]             {},
+                  [:Patient :birthDate]               {},
+                  [:Patient :communication]           {},
+                  [:Patient :communication :language] {:required true, :valueset {:id "simple-language"}},
+                  [:Patient :identifier]              {:minItems 1},
+                  [:Patient :identifier :system]      {:required true},
+                  [:Patient :identifier :value]       {:required    true,
+                                                       :description "The value that is unique within the system"},
+                  [:Patient :extension:birthsex]      {:sliceName           "birthsex",
+                                                       :extension-reference "birthsex"},
+                  [:Patient :telecom]                 {},
+                  [:Patient :telecom :value]          {:required true},
+                  [:Patient :telecom :system]         {:required true, :valueset {:id "fhir:contact-point-system"}},
+                  [:Patient :gender]                  {:valueset {:id "fhir:administrative-gender"}}}))
 
   (testing "domain resource parsing"
     (matcho/match
-      (prof/domain-resource->structure-definition (into (ordered-map []) domain-resource))
-      [(ordered-map [[:id [:Patient :extension :extension1]]
-                     [:path [:Patient :extension :extension1]]
-                     [:mustSupport true]
-                     [:sliceName "extension1"]
-                     [:min 0]
-                     [:max "1"]
-                     [:type [{:code    "Extension"
-                              :profile ["http://hl7.org/fhir/us/core/StructureDefinition/extension1"]}]]
-                     [:mapping [{:identity "argonaut-dq-dstu2" :map [:Patient :extension :extension1]}]]])
-       (ordered-map [[:id "Patient.identifier"]
-                     [:path "Patient.identifier"]
-                     [:mustSupport true]
-                     [:min 1]])
-       (ordered-map [[:id "Patient.identifier.system"]
-                     [:path "Patient.identifier.system"]
-                     [:mustSupport true]
-                     [:min 1]])
-       (ordered-map [[:id "Patient.identifier.value"]
-                     [:path "Patient.identifier.value"]
-                     [:mustSupport true]
-                     [:min 1]])
-       (ordered-map [[:id "Patient.name"]
-                     [:path "Patient.name"]
-                     [:mustSupport true]
-                     [:min 1]])
-       (ordered-map [[:id "Patient.name.family"]
-                     [:path "Patient.name.family"]
-                     [:mustSupport true]])
-       (ordered-map [[:id "Patient.name.given"]
-                     [:path "Patient.name.given"]
-                     [:mustSupport true]])]))
-
-
-  (def extension-diff
-    {[:Extension :race]              {:description "US Core Race Extension"}
-     [:Extension :race :text]        {:required    true
-                                      :description "Race Text"
-                                      :type        "string"}
-     [:Extension :race :ombCategory] {:collection true
-                                      :minItems   1
-                                      :type       "Coding"
-                                      :valueset   {:id "omb-race-category"}}
-
-     [:Extension :race :detailed]    {:collection  true
-                                      :description "Extended race codes"
-                                      :type        "Coding"
-                                      :valueset    {:id "detailed-race"}}})
-
-  (testing "extension diff parsing"
-    (matcho/match
-      (prof/extension-diff->structure-definition (into (ordered-map []) extension-diff))
-      [{:id         "Extension"
-        :path       "Extension"
-        :short      "Extension"
-        :definition "US Core Race Extension"
-        :min        0
-        :max        "1"}
-       {:id       "Extension.url"
-        :path     "Extension.url"
-        :min      1
-        :max      "1"
-        :type     [{:code "uri"}]
-        :fixedUri "race"}
-       {:id   "Extension.value[x]"
-        :path "Extension.value[x]"
-        :min  0
-        :max  "0"}
-       {:id          "Extension.extension:text"
-        :path        "Extension.extension"
-        :sliceName   "text"
-        :min         0
-        :max         "1"
-        :type        [{:code "Extension"}]
-        :mustSupport true
-        :mapping     [{:map "race"}]}
-       {:id       "Extension.extension:text.url"
-        :path     "Extension.extension.url"
-        :min      1 :max "1"
-        :type     [{:code "uri"}]
-        :fixedUri "race"}
-       {:id      "Extension.extension:text.valueString"
-        :path    "Extension.extension.valueString"
-        :min     1
-        :max     "1"
-        :type    [{:code "String"}]
-        :binding {:strength    true
-                  :description "Race Text"
-                  :valueSet    "http://hl7.org/fhir/us/core/ValueSet/omb-race-category"}}
-       {:id          "Extension.extension:ombCategory"
-        :path        "Extension.extension"
-        :sliceName   "ombCategory"
-        :min         0
-        :max         "1"
-        :type        [{:code "Extension"}]
-        :mustSupport true
-        :mapping     [{:map "race"}]}
-       {:id       "Extension.extension:ombCategory.url"
-        :path     "Extension.extension.url"
-        :min      1 :max "1"
-        :type     [{:code "uri"}]
-        :fixedUri "race"}
-       {:id      "Extension.extension:ombCategory.valueCoding"
-        :path    "Extension.extension.valueCoding"
-        :min     1
-        :max     "1"
-        :type    [{:code "Coding"}]
-        :binding {:strength    nil
-                  :description nil
-                  :valueSet    "http://hl7.org/fhir/us/core/ValueSet/omb-race-category"}}
-       {:id          "Extension.extension:detailed"
-        :path        "Extension.extension"
-        :sliceName   "detailed"
-        :min         0
-        :max         "1"
-        :type        [{:code "Extension"}]
-        :mustSupport true
-        :mapping     [{:map "race"}]}
-       {:id       "Extension.extension:detailed.url"
-        :path     "Extension.extension.url"
-        :min      1
-        :max      "1"
-        :type     [{:code "uri"}]
-        :fixedUri "race"}
-       {:id      "Extension.extension:detailed.valueCoding"
-        :path    "Extension.extension.valueCoding"
-        :min     1
-        :max     "1"
-        :type    [{:code "Coding"}]
-        :binding {:strength    nil
-                  :description "Extended race codes"
-                  :valueSet    "http://hl7.org/fhir/us/core/ValueSet/omb-race-category"}}]))
-  )
+      (prof/translate
+        json-rule/agenda
+        :prof-id
+        (into (ordered-map []) domain-resource))
+      [{:id   "Patient.address.postalCode",
+        :path "Patient.address.postalCode"}
+       {:id   "Patient.identifier.system",
+        :path "Patient.identifier.system",
+        :min  1}
+       {:id   "Patient.telecom.value",
+        :path "Patient.telecom.value",
+        :min  1}
+       {:id   "Patient.address",
+        :path "Patient.address"}
+       {:id      "Patient.address.state",
+        :path    "Patient.address.state",
+        :binding {:strength "required",
+                  :valueSet "http://hl7.org/fhir/ValueSet/administrative-gender"}}
+       {:id   "Patient.name.given",
+        :path "Patient.name.given"}
+       {:id        "Patient.extension:birthsex",
+        :path      "Patient.extension:birthsex",
+        :sliceName "birthsex",
+        :type      [{:code    "Extension",
+                     :profile ["http://hl7.org/fhir/us/core/StructureDefinition/prof-id-birthsex"]}]}
+       {:id   "Patient.address.line",
+        :path "Patient.address.line"}
+       {:id   "Patient.identifier",
+        :path "Patient.identifier",
+        :min  1}
+       {:id        "Patient.extension:race",
+        :path      "Patient.extension:race",
+        :sliceName "race",
+        :type      [{:code    "Extension",
+                     :profile ["http://hl7.org/fhir/us/core/StructureDefinition/prof-id-race"]}]}
+       {:id      "Patient.gender",
+        :path    "Patient.gender",
+        :binding {:strength "required",
+                  :valueSet "http://hl7.org/fhir/ValueSet/administrative-gender"}}
+       {:id   "Patient.identifier.value",
+        :path "Patient.identifier.value",
+        :min  1}
+       {:id "Patient.telecom", :path "Patient.telecom"}
+       {:id      "Patient.communication.language",
+        :path    "Patient.communication.language",
+        :binding {:strength "required",
+                  :valueSet "http://hl7.org/fhir/ValueSet/administrative-gender"},
+        :min     1}
+       {:id "Patient.communication", :path "Patient.communication"}
+       {:id "Patient.address.city", :path "Patient.address.city"}
+       {:id      "Patient.telecom.system",
+        :path    "Patient.telecom.system",
+        :binding {:strength "required",
+                  :valueSet "http://hl7.org/fhir/ValueSet/administrative-gender"},
+        :min     1}
+       {:id "Patient.birthDate", :path "Patient.birthDate"}
+       {:id "Patient.name.family", :path "Patient.name.family"}
+       {:id "Patient.name", :path "Patient.name", :min 1}])))
 
